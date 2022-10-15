df7276a7fbb0d2d8512ed3af32a0adb427369e03
felipehernandez98 <afelipehernandezdev@gmail.com>
2022-10-15 14:21:36 -0500

    modificando el prefijo de la api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v2') //Para colocar un prefijo global -> Ahora a todos 
+  //los modulos se debe inglresar con el localhost:3000/api/v2/pokemon
   await app.listen(3000);
 }
 bootstrap();
