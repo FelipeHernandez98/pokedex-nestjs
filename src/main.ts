e6dd3bdd99d7de1401b9341117b8d344e5fba56b
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2022-10-21 23:14:51 -0500

    creando el dto para crear pokemon y haciendo respectivas validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v2') //Para colocar un prefijo global -> Ahora a todos 
   //los modulos se debe inglresar con el localhost:3000/api/v2/pokemon
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
