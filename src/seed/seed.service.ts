ded2a9ef9750db75da67ad9ef01adb8409d64030
felipehernandez98 <afelipehernandezdev@gmail.com>
2022-10-26 15:50:55 -0500

    haciendo la inserccion en la BD con promesas simultaneas

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,13 +17,25 @@ export class SeedService {
   ) { }
 
   async executeSeed() {
+
+    this.pokemonModel.deleteMany({});//Nos encargamos de eliminar lo que haya en la BD
+
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
+    const insertPromisesArray = [];
+
     data.results.forEach(async ({ name, url }) => {
+
       const segments = url.split('/');// cortamos la url por "/" para extraer el id del pokemon
       const no: number = +segments[segments.length - 2]; // convertimos a numero con el +, y buscamos en el array la penultima posición donde esta el id
-      const pokeom = await this.pokemonModel.create({ name, no });
+
+      insertPromisesArray.push(
+        this.pokemonModel.create({ name, no })
+      );
+      //const pokeom = await this.pokemonModel.create({ name, no });
     })
+
+    await Promise.all(insertPromisesArray);
     return 'Seed ejecutado correctamente!';
   }
 }
