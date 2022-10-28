4ac92c86e86436a64e67f465bc38d732c042941b
felipehernandez98 <afelipehernandezdev@gmail.com>
2022-10-28 14:23:05 -0500

    configurando las variables de entorno en un archivo de configuración y pasarlas al modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,20 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
 
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
 
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -25,6 +28,6 @@ import { ConfigModule } from '@nestjs/config';
 export class AppModule {
 
   constructor() {
-    console.log(process.env)
+    console.log(process.env.PORT)
   }
 }
